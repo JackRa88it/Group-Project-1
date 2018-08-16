8eb193e02e0725bd400b49a744a76fa918ec4e7a
Jack <jhkent88@gmail.com>
2018-08-16 13:05:52 -0700

    Fbase user signin working, ready to test notes

--- assets/javascript/notes.js
+++ assets/javascript/notes.js
@@ -1,7 +1,10 @@
 // this js file is just for the personal notes/checklists feature
-// made by Jack
+// made by Jack K
 
-// 
+// GLOBAL VARIABLES
+// ----------------------
+
+// initialize firebase
 var config = {
   apiKey: "AIzaSyD8jTGozVGNENJk8JbzPb22Z_6S2fUc_m8",
   authDomain: "group-project-1-6eee4.firebaseapp.com",
@@ -11,3 +14,81 @@ var config = {
   messagingSenderId: "1034102183779"
 };
 firebase.initializeApp(config);
+var database = firebase.database();
+
+
+
+// FUNCTIONS
+// ----------------------
+
+
+
+
+
+// EVENT HANDLERS/CALLS
+// ----------------------
+
+// sign up new users
+$('#SignUpButton').on('click', function(event) {
+
+  event.preventDefault();
+
+  console.log('sign up clicked');
+
+  var email = $('#InputEmail').val().trim();
+  var password = $('#InputPassword').val().trim();
+
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log('error');
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+  });
+
+});
+
+// sign in existing users
+$('#SignUpButton').on('click', function(event) {
+
+  event.preventDefault();
+
+  var email = $('#InputEmail').val().trim();
+  var password = $('#InputPassword').val().trim();
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+
+});
+
+// authentication state observer gets user data
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    console.log(displayName);
+    console.log(email);
+    console.log(emailVerified);
+    console.log(photoURL);
+    console.log(isAnonymous);
+    console.log(uid);
+    console.log(providerData);
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
