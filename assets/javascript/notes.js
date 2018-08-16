4092b979743192b9161b80d00914f0cbcbbe386b
Jack <jhkent88@gmail.com>
2018-08-16 10:38:06 -0700

    Link js libraries in html, initialize dbase in notes.js

--- assets/javascript/notes.js
+++ assets/javascript/notes.js
@@ -1,3 +1,13 @@
 // this js file is just for the personal notes/checklists feature
 // made by Jack
 
+// 
+var config = {
+  apiKey: "AIzaSyD8jTGozVGNENJk8JbzPb22Z_6S2fUc_m8",
+  authDomain: "group-project-1-6eee4.firebaseapp.com",
+  databaseURL: "https://group-project-1-6eee4.firebaseio.com",
+  projectId: "group-project-1-6eee4",
+  storageBucket: "",
+  messagingSenderId: "1034102183779"
+};
+firebase.initializeApp(config);
