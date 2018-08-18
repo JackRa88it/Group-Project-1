869b8bd108ffde990612d1f6a5f2a0234144a288
Elizabeth <avrora7@yahoo.com>
2018-08-18 13:27:38 -0700

    added js placeholder for all 4 topics to add content

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,6 +7,12 @@ let headers = {
     "notes": "Notes"
 }
 
+let updateDateTime = function () {
+    $("#dateTime").text(moment().format('LLLL'))
+}
+updateDateTime();
+setInterval(updateDateTime, 1000);
+
 let selectPanel = function (menuItem) {
     $("#navPanels div").show();
     $("#detailPanels div").hide();
@@ -26,6 +32,7 @@ let activateDetailView = function (menuItem) {
 let activateHomePage = function () {
     $(".container").show();
     $(".container-fluid").hide();
+    populateAll();
 }
 
 $('#backBtn').on("click", function () {
@@ -34,32 +41,76 @@ $('#backBtn').on("click", function () {
 
 $("#navPanels .weather").on("click", function () {
     selectPanel("weather");
+    populateWeather()
 });
 
 $(".container .weather").on("click", function () {
     activateDetailView("weather");
+    populateWeather()
 });
 
 $("#navPanels .news").on("click", function () {
     selectPanel("news")
+    populateNews();
 });
 
 $(".container .news").on("click", function () {
     activateDetailView("news")
+    populateNews();
 });
 
 $("#navPanels .traffic").on("click", function () {
     selectPanel("traffic")
+    populateTraffic();
 });
 
 $(".container .traffic").on("click", function () {
     activateDetailView("traffic")
+    populateTraffic();
 });
 
 $("#navPanels .notes").on("click", function () {
-    selectPanel("notes")
+    selectPanel("notes");
+    populateNotes();
 });
 
 $(".container .notes").on("click", function () {
     activateDetailView("notes")
+    populateNotes();
 });
+
+
+$(function () {
+    var navMain = $(".navbar-collapse");
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+
+    populateAll()
+});
+
+
+/* functions that need to be implemented by ajax for all topics */
+let populateWeather = function () {
+    $("#weather, #detailPanels .weather").text("45F")
+}
+
+let populateNews = function () {
+    $("#news, #detailPanels .news").text("IBM is down by 3%")
+}
+
+let populateTraffic = function () {
+    $("#traffic, #detailPanels .traffic").text("Today you will succeed")
+}
+
+let populateNotes = function () {
+    $("#notes, #detailPanels .notes").html("<ul><li>Visit deprivation room</li><li>Buy milk</li></ul>")
+}
+
+let populateAll = function () {
+    populateWeather();
+    populateNews();
+    populateTraffic();
+    populateNotes();
+}
+
